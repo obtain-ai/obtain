bbe40dc793702ccb2981fbc9ac85cab476e86e7e
inwia <ivyvines127@gmail.com>
2025-10-18 16:35:12 -0400

    Update content in +page.svelte for clarity

--- src/routes/promptify/+page.svelte
+++ src/routes/promptify/+page.svelte
@@ -7,8 +7,7 @@
 		Why this Matters:
 	{/snippet}
 	{#snippet content()}
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-		labore et dolore magna aliqua.
+		Promptify helps you unlock the full potential of AI by learning how to write clear, effective prompts. Chatbots often aren’t utilized to their full potential because the prompts they receive are too vague or incomplete. By improving your prompts, you’ll learn to communicate with AI more precisely and get smarter, more useful answers every time.
 	{/snippet}
 </InfoDisplay>
 
