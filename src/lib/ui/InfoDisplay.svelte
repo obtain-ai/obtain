71112921bbdec8050e5637457871af77c84ce26b
inwia <ivyvines127@gmail.com>
2025-10-18 18:38:32 -0400

    Clean up InfoDisplay by removing unused SVG
    Removed commented-out SVG code from InfoDisplay component.

--- src/lib/ui/InfoDisplay.svelte
+++ src/lib/ui/InfoDisplay.svelte
@@ -7,37 +7,6 @@
 >
 	{#if title}
 		<span class="flex flex-row items-center gap-2 font-bold text-lg">
-			<!--
-			<svg
-				width="18px"
-				height="18px"
-				stroke-width="1.5"
-				viewBox="0 0 24 24"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-				color="#e4e4e7"
-				><path
-					d="M12 11.5V16.5"
-					stroke="#e4e4e7"
-					stroke-width="1.5"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				></path><path
-					d="M12 7.51L12.01 7.49889"
-					stroke="#e4e4e7"
-					stroke-width="1.5"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				></path><path
-					d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-					stroke="#e4e4e7"
-					stroke-width="1.5"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				></path></svg
-			-->
-			>
-
 			{@render title()}
 		</span>
 	{/if}
