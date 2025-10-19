5f5c230396930d57fd7ce835bd04c6cf20184778
caitlincg <caitlinchen2008@gmail.com>
2025-10-19 18:06:04 -0400

    Update InfoDisplay.svelte

--- src/lib/ui/InfoDisplay.svelte
+++ src/lib/ui/InfoDisplay.svelte
@@ -2,14 +2,14 @@
   let { title, content } = $props();
 </script>
 
-<div class="mx-auto mb-4 flex w-[80%] flex-col gap-2 rounded-md border border-zinc-200 bg-zinc-100 p-6 shadow-sm
+<div class="mx-auto mb-4 flex w-[80%] flex-col gap-2 rounded-md border border-zinc-700 bg-zinc-800 p-6 shadow-sm
             dark:border-zinc-700 dark:bg-zinc-800 transition-colors duration-300">
   {#if title}
-    <span class="flex flex-row items-center gap-2 font-bold text-lg text-zinc-900 dark:text-white">
+    <span class="flex flex-row items-center gap-2 font-bold text-lg text-white dark:text-white">
       {@render title()}
     </span>
   {/if}
-  <p class="text-zinc-700 dark:text-zinc-300 leading-relaxed">
+  <p class="text-zinc-300 dark:text-zinc-300 leading-relaxed">
     {@render content()}
   </p>
 </div>
