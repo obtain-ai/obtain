3fe294c28f6a215fafed12d846e2bfdea2587caa
inwia <ivyvines127@gmail.com>
2025-10-19 22:39:22 -0400

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,12 +11,12 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="relative min-h-[100vh] overflow-y-auto bg-zinc-800 text-zinc-200">
+<div class="relative min-h-screen overflow-y-auto bg-zinc-800 text-zinc-200">
 	{#key page.url.pathname}
 		<div
 			in:fly={{ x: 300, duration: 300 }}
 			out:fly={{ x: -300, duration: 300 }}
-			class="absolute inset-0"
+			class="relative"
 		>
 			<div class="relative flex items-center justify-center">
 				{#if page.url.pathname != '/'}
