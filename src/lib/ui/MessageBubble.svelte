b1614fde1f7e51938f107e9c2d7fa6c2c37b8295
inwia <ivyvines127@gmail.com>
2025-10-27 16:47:52 -0400

    tighten the spacing

--- src/lib/ui/MessageBubble.svelte
+++ src/lib/ui/MessageBubble.svelte
@@ -37,41 +37,51 @@
   :global(.prose h1) {
     font-size: 1.5rem;
     font-weight: bold;
-    margin-top: 1rem;
-    margin-bottom: 0.5rem;
+    margin-top: 0.75rem;
+    margin-bottom: 0.25rem;
   }
   
   :global(.prose h2) {
     font-size: 1.25rem;
     font-weight: bold;
-    margin-top: 1rem;
-    margin-bottom: 0.5rem;
+    margin-top: 0.75rem;
+    margin-bottom: 0.25rem;
   }
   
   :global(.prose h3) {
     font-size: 1.1rem;
     font-weight: bold;
-    margin-top: 0.75rem;
-    margin-bottom: 0.5rem;
+    margin-top: 0.5rem;
+    margin-bottom: 0.25rem;
   }
   
   :global(.prose strong) {
     font-weight: bold;
   }
   
+  :global(.prose p) {
+    margin-bottom: 0.5rem;
+  }
+  
   :global(.prose ul) {
     list-style-type: disc;
-    margin-left: 1rem;
+    margin-left: 1.5rem;
+    margin-top: 0.25rem;
     margin-bottom: 0.5rem;
   }
   
   :global(.prose ol) {
     list-style-type: decimal;
-    margin-left: 1rem;
+    margin-left: 1.5rem;
+    margin-top: 0.25rem;
     margin-bottom: 0.5rem;
   }
   
   :global(.prose li) {
-    margin-bottom: 0.25rem;
+    margin-bottom: 0.1rem;
+  }
+  
+  :global(.prose hr) {
+    margin: 0.5rem 0;
   }
 </style>
