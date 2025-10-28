016a11ece421e8871dd6c6018eed1e982cee198a
caitlincg <caitlinchen2008@gmail.com>
2025-10-28 15:01:07 -0400

    horizontal lines

--- src/lib/ui/InfoDisplay.svelte
+++ src/lib/ui/InfoDisplay.svelte
@@ -2,15 +2,14 @@
 	let { title, content } = $props();
 </script>
 
-<div
-	class="mx-auto mb-4 flex w-[80%] flex-col gap-2 rounded-md border-1 border-zinc-200 bg-zinc-700 p-4"
->
+<div class="mx-auto mb-4 w-[80%]">
 	{#if title}
-		<span class="flex flex-row items-center gap-2 font-bold text-lg">
+		<span class="flex flex-row items-center gap-2 font-bold text-lg text-zinc-200 mb-2">
 			{@render title()}
 		</span>
 	{/if}
-	<p>
+	<p class="text-zinc-300 mb-4">
 		{@render content()}
 	</p>
+	<hr class="border-zinc-600">
 </div>
