aa1a75b4a4bd6dea6817bf21df4af066921e48b0
inwia <ivyvines127@gmail.com>
2025-10-29 22:12:22 -0400

    rename from secret_openai_api_key to openai_api_key

--- src/lib/server/openai/openai.ts
+++ src/lib/server/openai/openai.ts
@@ -1,8 +1,8 @@
 import OpenAI from 'openai';
-import { SECRET_OPENAI_API_KEY } from '$env/static/private';
+import { OPENAI_API_KEY } from '$env/static/private';
 
 const openai = new OpenAI({
-	apiKey: SECRET_OPENAI_API_KEY
+	apiKey: OPENAI_API_KEY
 });
 
 export { openai };
