9dfa78c51b1ff7f925c704de35257bfafe2f52ed
inwia <ivyvines127@gmail.com>
2026-02-05 18:14:46 -0500

    reformat tips into bullet points

--- src/routes/promptify/+page.svelte
+++ src/routes/promptify/+page.svelte
@@ -53,7 +53,11 @@
 					Tips!
 				{/snippet}
 				{#snippet content()}
-					Make sure your prompt includes context, what your goal is, how you want your prompt to be executed, and details!
+					Make sure your prompt includes:<br><br>
+						• Context<br>
+						• What your goal is<br>
+						• How you want your prompt to be executed<br>
+						• Details<br>
 				{/snippet}
 			</InfoDisplay>
 		</div>
