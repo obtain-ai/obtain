f51e8c58c40568667a45b692b6bbadd062fce529
caitlincg <caitlinchen2008@gmail.com>
2026-02-05 18:06:38 -0500

    Move back button down

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -41,7 +41,7 @@
 		<div in:fly={{ x: 300, duration: 300 }} out:fly={{ x: -300, duration: 300 }} class="relative">
 			<div class="relative flex items-center justify-center">
 				{#if $page.url.pathname != '/'}
-					<div class="absolute left-8">
+					<div class="absolute left-8 top-20">
 						<BackButton href="/" />
 					</div>
 				{/if}
